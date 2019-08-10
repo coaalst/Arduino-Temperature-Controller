7bad27908fe4ae83e468c65ba198ba1bff713585
Aleksandar Stojanovic <astojanovic17@raf.rs>
2019-08-10 19:33:05 +0200

    fan control

--- dispay_temp_v1/dispay_temp_v1.ino
+++ dispay_temp_v1/dispay_temp_v1.ino
@@ -12,8 +12,9 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 //Data linija senzorana povezana na pin 2
 #define SENSOR_DATA_BUS 2
 
-//Pin za napajanje senzora
-#define SENSOR_POWER_LINE 4
+//Pinovi za korntolu releja
+#define relay1 3
+#define relay2 4
 
 //Setup za oneWire instancu koja ce komunicirati sa svim OneWire uredjajima
 OneWire oneWire(SENSOR_DATA_BUS);
@@ -24,14 +25,18 @@ DallasTemperature sensors(&oneWire);
 //soil senzor config
 int SENSOR_PIN = A0;
 
+
 void setup() {
   //Pokreni lib za ekran
   u8g2.begin();
   u8g2.setFont(u8g2_font_crox4hb_tr);
 
-  //konfig power pin-a
-  pinMode(SENSOR_POWER_LINE, OUTPUT);
-  digitalWrite(SENSOR_POWER_LINE, HIGH);
+  //config releja
+  pinMode(relay1, OUTPUT);
+  digitalWrite(relay1, HIGH);
+
+  pinMode(relay2, OUTPUT);
+  digitalWrite(relay2, HIGH);
   
   Serial.begin(9600);
 
@@ -47,7 +52,7 @@ void setup() {
  * Troubleshoot preko serial monitora
  */
 void loop() {
-  //Float za temp prima vrednost
+//Float za temp prima vrednost
  float temp_val = read_temperature();
 
   //Ako smo uspesno procitali
@@ -55,17 +60,19 @@ void loop() {
 
    //Pravimo char array za drawStr funkciju
    String temp_string = String(temp_val);
+   if(temp_val>= 29)digitalWrite(relay1, HIGH);
+   else digitalWrite(relay1, LOW);
    char buffer[5];
    temp_string.toCharArray(buffer,5);
-   
-   //Ispis na ekran
-   display_out(buffer);
-  } 
 
+   //Ispis na ekran
+   display_out(buffer);  
+  } 
+ 
   //Exception
   else Serial.println("Greska prilikom citanja!");
 
-  Serial.print("Hvatam temperaturu");
+  Serial.print("Hvatam vlagu");
   
   //citanje vlaznosti
   int soil_val = analogRead(SENSOR_PIN);
@@ -76,10 +83,8 @@ void loop() {
   //ispis
   Serial.print("Vlaga: ");
   Serial.print(soil_val);
-
-  delay(5000);
-
- 
+  
+  delay(2000);
 }
 
 /*
@@ -104,13 +109,13 @@ float read_temperature(){
 /*
  * funkcija za ispis na OLED ekran
  */
-void display_out(char text_buffer) {
+void display_out(String text_buffer) {
   //Obrisi
   u8g2.clearBuffer();
-
+  
   //Ispis
   u8g2.drawStr(0, 15, "Temperatura:");
-  u8g2.drawStr(0, 30, buffer);
+  u8g2.drawStr(0, 30, text_buffer.c_str());
   u8g2.drawStr(35, 30, " C");
   u8g2.sendBuffer();
 }
